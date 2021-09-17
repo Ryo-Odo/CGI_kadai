18a419d1b27aad20a23b12286a398f5101b6d203
Ryo_Odo <marukae@gmail.com>
2021-09-17 07:20:13 +0000

    課題作成

---
+++ web_tech/test.rb
@@ -0,0 +1,20 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+server.mount('/kadai_1.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_1.rb')
+server.mount('/kadai_2.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_2.rb')
+
+server.start
